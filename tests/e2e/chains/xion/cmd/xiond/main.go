ce4b93b89d4efbe302aaf2f107ce89987a114411
GnaD13 <cpt.meoz@gmail.com>
2025-01-08 19:32:08 +0700

    debug enclaves

--- tests/e2e/chains/xion/cmd/xiond/main.go
+++ tests/e2e/chains/xion/cmd/xiond/main.go
@@ -8,10 +8,16 @@ import (
 	svrcmd "github.com/cosmos/cosmos-sdk/server/cmd"
 
 	"github.com/datachainlab/cosmos-ethereum-ibc-lcp/tests/e2e/chains/xion/app"
+	"github.com/datachainlab/lcp-go/sgx/ias"
 )
 
 func main() {
 	rootCmd, _ := NewRootCmd()
+
+	// WARNING: if you use the simd in production, you must remove the following code:
+	ias.SetAllowDebugEnclaves()
+	defer ias.UnsetAllowDebugEnclaves()
+	// END WARNING
 
 	if err := svrcmd.Execute(rootCmd, "", app.DefaultNodeHome); err != nil {
 		log.NewLogger(rootCmd.OutOrStderr()).Error("failure when running app", "err", err)
