ca61d84818fadf12eaaf2ec9366d8a70f49cf9c5
Masanori Yoshida <masanori.yoshida@datachain.jp>
2025-03-04 00:10:07 +0900

    fix yrly to add the cosmwasm prover
    Signed-off-by: Masanori Yoshida <masanori.yoshida@datachain.jp>

--- relayer/main.go
+++ relayer/main.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"os"
 
+	cw "github.com/datachainlab/cosmwasm-ibc-relay-prover/relay"
+	cwtm "github.com/datachainlab/cosmwasm-ibc-relay-prover/relay/tendermint"
 	"github.com/datachainlab/ethereum-ibc-relay-chain/pkg/relay/ethereum"
 	"github.com/datachainlab/ibc-hd-signer/pkg/hd"
 	parlia "github.com/datachainlab/ibc-parlia-relay/module"
@@ -22,6 +24,8 @@ func main() {
 		hd.Module{},
 		lcp.Module{},
 		lcptm.Module{},
+		cw.Module{},
+		cwtm.Module{},
 		rawsigner.Module{},
 	); err != nil {
 		fmt.Fprintln(os.Stderr, "Error:", err)
