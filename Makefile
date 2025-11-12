1c1f6ccb408d20216af4812e78a7d31e8a78d86c
yoshidan <naohiro.y@gmail.com>
2025-11-12 09:13:29 +0900

    add run service

--- Makefile
+++ Makefile
@@ -175,6 +175,10 @@ build-images:
 e2e-test: e2e-clean $(LCP_BIN) $(Signed_RustEnclave_Name) yrly
 	LCP_BIN=$(LCP_BIN) ./tests/e2e/scripts/run_e2e_test.sh $(E2E_OPTIONS)
 
+.PHONY: e2e-service
+e2e-service: e2e-clean $(LCP_BIN) $(Signed_RustEnclave_Name) yrly
+	LCP_BIN=$(LCP_BIN) ./tests/e2e/scripts/run_service.sh $(E2E_OPTIONS)
+
 .PHONY: e2e-clean
 e2e-clean:
 	$(MAKE) -C ./tests/e2e/chains/bsc rm-oz-upgrades
---
+++ tests/e2e/scripts/run_service.sh
@@ -0,0 +1,107 @@
+#!/usr/bin/env bash
+set -ex
+
+# Usage: run_e2e_test.sh <--no_run_lcp> <--zkdcap|--mock_zkdcap> <--enclave_debug> <--upgrade_test> <--key_expiration=<integer>>
+
+source $(cd $(dirname "$0"); pwd)/util
+
+E2E_TEST_DIR=./tests/e2e/cases/tm2bsc
+export NO_RUN_LCP=false
+export LCP_ENCLAVE_DEBUG=0
+export LCP_KEY_EXPIRATION=86400
+# LCP_RISC0_IMAGE_ID must be set to the same value as in the LCP service
+# e.g. https://github.com/datachainlab/zkdcap/blob/fd44cfc9718a0bd4a58f5dbf2b0b89c25144893d/zkvm/risc0/src/methods.rs#L3
+LCP_RISC0_IMAGE_ID=${LCP_RISC0_IMAGE_ID:-0xe5056aa7a8064abeb648b31d5efa8697a79d416b937cb917d1428cec91a56c67}
+export ZKDCAP=false
+export LCP_ZKDCAP_RISC0_MOCK=false
+export LCP_RISC0_IMAGE_ID
+export USE_UPGRADE_TEST=no
+CERTS_DIR=./tests/certs
+ARGS=$(getopt -o '' --long no_run_lcp,enclave_debug,zkdcap,mock_zkdcap,upgrade_test,key_expiration: -- "$@")
+eval set -- "$ARGS"
+while true; do
+    case "$1" in
+        --no_run_lcp)
+            echo "Skip running LCP"
+            NO_RUN_LCP=true
+            shift
+            ;;
+        --enclave_debug)
+            echo "Enclave debug enabled"
+            LCP_ENCLAVE_DEBUG=1
+            shift
+            ;;
+        --zkdcap)
+            echo "ZKDCAP enabled"
+            ZKDCAP=true
+            LCP_ZKDCAP_RISC0_MOCK=false
+            shift
+            ;;
+        --mock_zkdcap)
+            echo "Mock ZKDCAP enabled"
+            ZKDCAP=true
+            LCP_ZKDCAP_RISC0_MOCK=true
+            shift
+            ;;
+        --key_expiration)
+            echo "Key expiration set to $2"
+            LCP_KEY_EXPIRATION=$2
+            shift 2
+            ;;
+        --upgrade_test)
+            echo "Enable upgrade test"
+            USE_UPGRADE_TEST=yes
+            shift
+            ;;
+        --)
+            shift
+            break
+            ;;
+        *)
+            echo "Internal error!"
+            exit 1
+            ;;
+    esac
+done
+
+if [ "$NO_RUN_LCP" = "false" ]; then
+    echo "Run LCP for testing"
+    LCP_BIN=${LCP_BIN:-./bin/lcp}
+    LCP_ENCLAVE_PATH=${LCP_ENCLAVE_PATH:-./bin/enclave.signed.so}
+    export LCP_MRENCLAVE=$(${LCP_BIN} enclave metadata --enclave=${LCP_ENCLAVE_PATH} | jq -r .mrenclave)
+    LCP_BIN=${LCP_BIN} LCP_ENCLAVE_PATH=${LCP_ENCLAVE_PATH} ./tests/e2e/scripts/init_lcp.sh
+    ${LCP_BIN} --log_level=info service start --enclave=${LCP_ENCLAVE_PATH} --address=127.0.0.1:50051 --threads=2 &
+    LCP_PID=$!
+    if [ "$SGX_MODE" = "SW" ]; then
+        export LCP_RA_ROOT_CERT_HEX=$(cat ${CERTS_DIR}/root.crt | xxd -p -c 1000000)
+        export LCP_DCAP_RA_ROOT_CERT_HEX=$(cat ${CERTS_DIR}/simulate_dcap_root_cert.pem | xxd -p -c 1000000)
+    fi
+else
+    echo "Skip running LCP"
+    echo "We assume that LCP is running with the HW mode"
+    res=$(grpcurl -plaintext 127.0.0.1:50051 lcp.service.enclave.v1.Query.EnclaveInfo)
+    enclave_debug=$(echo $res | jq -r .enclaveDebug)
+    if [ "$enclave_debug" == "true" ]; then
+        if [ "$LCP_ENCLAVE_DEBUG" == "0" ]; then
+            echo "Remote LCP's enclave debug is enabled, but LCP_ENCLAVE_DEBUG is not set"
+            exit 1
+        fi
+    else
+        if [ "$LCP_ENCLAVE_DEBUG" == "1" ]; then
+            echo "Remote LCP's enclave debug is disabled, but LCP_ENCLAVE_DEBUG is set"
+            exit 1
+        fi
+    fi
+    export LCP_MRENCLAVE=0x$(echo $res | jq -r .mrenclave | base64 -d | xxd -p | tr -d $'\n')
+fi
+
+make -C ${E2E_TEST_DIR} network
+
+
+E2E_TEST_DIR=${E2E_TEST_DIR} ${E2E_TEST_DIR}/scripts/gen_rly_config.sh
+
+# wait 2000 blocks
+sleep 900
+make -C ${E2E_TEST_DIR} setup handshake
+
+make -C ${E2E_TEST_DIR} service
