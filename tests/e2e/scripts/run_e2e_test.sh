7a19aab0adc7b6194eeaec8bf5f6d90d7e2f3c0a
yoshidan <naohiro.y@gmail.com>
2026-01-05 14:36:02 +0900

    update

--- tests/e2e/scripts/run_e2e_test.sh
+++ tests/e2e/scripts/run_e2e_test.sh
@@ -100,8 +100,7 @@ make -C ${E2E_TEST_DIR} network
 
 E2E_TEST_DIR=${E2E_TEST_DIR} ${E2E_TEST_DIR}/scripts/gen_rly_config.sh
 
-# wait 2000 blocks
-sleep 900
+sleep 1000
 make -C ${E2E_TEST_DIR} setup handshake
 
 if [ $USE_UPGRADE_TEST = yes ]
