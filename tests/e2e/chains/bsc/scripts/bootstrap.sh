2e6d477947eb239ffb3c018a363522399d155916
yoshidan <naohiro.y@gmail.com>
2026-01-05 13:09:49 +0900

    check

--- tests/e2e/chains/bsc/scripts/bootstrap.sh
+++ tests/e2e/chains/bsc/scripts/bootstrap.sh
@@ -10,9 +10,12 @@ function prepare() {
     echo "geth do not exist!"
     exit 1
   fi
+  ls -ltr
   cp -R ${workspace}/genesis_source/ ${workspace}/genesis/
+  ls -ltr
   # run `make clean` to remove files
   cd ${workspace}/genesis
+  ls -ltr
   rm -rf validators.conf
 }
 
